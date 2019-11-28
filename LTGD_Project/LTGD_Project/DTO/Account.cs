81dd24bdb9b5307ac7a102a11b5433c2d6a9ef98
hobok32 <53633941+hobok32@users.noreply.github.com>
2019-11-28 11:52:38 +0700

    Add class Account

---
+++ LTGD_Project/LTGD_Project/DTO/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+namespace LTGD_Project.DTO
+{
+    class Account
+    {
+        public  string IdAccount { set; get; }
+        public string PasswordAccount { set; get; }
+        public string NameUser { set; get; }
+        public string PhoneNum { set; get; }
+        public string Role { set; get; }
+        public string ImgAccount { set; get; }
+
+        public Account(DataRow row)
+        {
+            this.IdAccount = (string)row["idAccount"];
+            this.PasswordAccount = (string)row["passwordAccount"];
+            this.NameUser = (string)row["nameUser"];
+            this.ImgAccount = (string)row["imgAccount"];
+            this.PhoneNum = (string)row["phoneNum"];
+            this.Role = (string)row["role"];
+        }
+    }
+}
