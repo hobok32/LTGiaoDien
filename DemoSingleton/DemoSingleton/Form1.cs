adc9476bdcada1cf4ec4d0c86aba7fb40654d751
hobok32 <53633941+hobok32@users.noreply.github.com>
2019-12-04 12:57:59 +0700

    Demo

---
+++ DemoSingleton/DemoSingleton/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoSingleton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //SingleTon singleTon = new SingleTon();//Tạo 1 thực thể mới
+            //SingleTon singleTon1 = new SingleTon();//Tạo 1 thực thể mới
+
+            SingleTon.Instance.Demo();//Sử dụng nhưng hàm trong class SingleTon thông qua duy nhất 1 thực thể Instance
+            SingleTon.Instance.Demo1();
+
+            //Muốn sử dụng class SingleTon chỉ có thể thông qua 1 thực thể duy nhất: Instance
+
+        }
+    }
+}
---
+++ DemoSingleton/DemoSingleton/SingleTon.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoSingleton
+{
+    class SingleTon
+    {
+        private static volatile SingleTon instance;//Tạo thực thể instance static + tránh đụng độ
+
+        static object key = new object();
+
+        public static SingleTon Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (key)//Tránh chiếm dụng tài nguyên
+                    {
+                        instance = new SingleTon();
+                    }
+                }
+                return instance;
+            }
+            //Chỉ có thể get chứ không được set
+        }
+
+        private SingleTon() { } //Ngăn tạo ra thực thể khác ở bất cứ nơi nào
+
+        public void Demo()
+        {
+            MessageBox.Show("DEMO");
+        }
+        public void Demo1()
+        {
+            MessageBox.Show("DEMO");
+        }
+    }
+}
